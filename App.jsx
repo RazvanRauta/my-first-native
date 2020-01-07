cd58d5bb9c53cec14092c2877649a8799414841a
razvanRauta <razvan.rauta@gmail.com>
2020-01-07 22:20:06 +0300

    Set up eslint and prettier

--- App.jsx
+++ App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Olea I love you &#10084; more!</Text>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%',
+    width: '100%',
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Natives!</Text>
+    </View>
+  );
+};
+
+export default App;
