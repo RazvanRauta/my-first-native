f84816fc14e7680c5e9fd0647aaec6de9132d8ff
razvanRauta <razvan.rauta@gmail.com>
2020-01-08 23:19:43 +0300

    Added some basic components and styles

--- App.jsx
+++ App.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
+  screen: {
+    padding: 50,
+  },
+  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%',
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 10,
   },
 });
 
-const App = () => {
+export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
   return (
-    <View style={styles.container}>
-      <Text>Hello Natives!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={enteredText => setEnteredGoal(enteredText)}
+          value={enteredGoal}
+        />
+        <Button
+          title="Add"
+          onPress={() =>
+            setCourseGoals(currentGoals => [
+              ...currentGoals,
+              { id: Math.random().toString(), value: enteredGoal },
+            ])
+          }
+        />
+      </View>
+      <FlatList
+        data={courseGoals}
+        keyExtractor={item => item.id}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
-};
-
-export default App;
+}
